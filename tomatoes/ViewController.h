a19c33af7669cf4903f7e86c9939ffb141d0543e
venkad <venkadesh.gm@gmail.com>
2014-01-15 20:31:15 -0800

    Initial Commit

---
+++ tomatoes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tomatoes
+//
+//  Created by Venkadeshkumar Dhandapani on 1/15/14.
+//  Copyright (c) 2014 Venkadeshkumar Dhandapani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ tomatoes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tomatoes
+//
+//  Created by Venkadeshkumar Dhandapani on 1/15/14.
+//  Copyright (c) 2014 Venkadeshkumar Dhandapani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
